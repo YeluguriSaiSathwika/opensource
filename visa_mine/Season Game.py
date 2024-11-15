06a0688e21e887fff51a39bc79479985ba4c139e
YeluguriSaiSathwika <161927609+YeluguriSaiSathwika@users.noreply.github.com>
2024-11-15 16:15:30 +0530

    Create Season Game.py

---
+++ visa_mine/Season Game.py
@@ -0,0 +1,11 @@
+n=int(input())
+if n==12 or n==1 or n==2:
+    print('Winter')
+elif n==11 or n==10 or n==9:
+    print('Autumn')
+elif n==8 or n==7 or n==6:
+    print('Summer')
+elif n==5 or n==4 or n==3:
+    print('Spring')
+else:
+    print('Invalid')
